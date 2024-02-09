248ff7859b5dbe79082ae0576bd91798695fe61c
Claudio Tomas <ccaducarlos@gmail.com>
2024-02-09 17:27:25 +0200

    feat: added focus and hover animations

--- src/App.tsx
+++ src/App.tsx
@@ -14,29 +14,9 @@ export function App() {
 			</form>
 			<div className="h-px bg-slate-700"></div>
 			<div className="grid grid-cols-3 auto-rows-[250px] gap-6">
-				<Card />
-				<div className="rounded-md bg-slate-800 p-5 space-y-3">
-					<p className="text-slate-200 text-sm font-medium ">
-						Adicionar nota
-					</p>
-					<p className="text-slate-400">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit.
-						Corrupti, atque consequatur? Ab aspernatur temporibus,
-						ullam quis doloremque quo! Ratione a voluptatem ullam
-						aliquid est iusto quod incidunt aspernatur, fugit
-						necessitatibus!
-					</p>
-				</div>
-				<div className="rounded-md bg-slate-800 p-5 space-y-3">
-					<p className="text-slate-200">...</p>
-					<p className="text-slate-400">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit.
-						Corrupti, atque consequatur? Ab aspernatur temporibus,
-						ullam quis doloremque quo! Ratione a voluptatem ullam
-						aliquid est iusto quod incidunt aspernatur, fugit
-						necessitatibus!
-					</p>
-				</div>
+				<Card time="" type="new" />
+				<Card time="há 4 dias" type="old" />
+				<Card time="há 4 dias" type="old" />
 			</div>
 		</div>
 	);
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,16 +1,28 @@
-export const Card = () => {
+interface CardProps {
+	type: "new" | "old";
+	time: string;
+}
+
+export const Card = ({ type, time }: CardProps) => {
+	const text_color = type === "new" ? "text-slate-200" : "text-slate-300";
+	const bg_color = type === "new" ? "bg-slate-700" : "bg-slate-800";
 	return (
-		<div className="rounded-md bg-slate-600 p-5 space-y-3">
-			<p className="text-slate-200 text-sm font-medium ">
-				Adicionar nota
+		<button
+			className={`text-left rounded-md ${bg_color} p-5 space-y-3 overflow-hidden relative  hover:ring-2 hover:ring-slate-600 focus-visible:ring-lime-400 focus-visible:ring-2 outline-none`}
+		>
+			<p className={`${text_color} text-sm font-medium`}>
+				{type === "new" ? "Adicionar nota" : time}
 			</p>
 			<p className="text-slate-400 leading-6 ">
 				Lorem ipsum dolor sit amet consectetur adipisicing elit.
 				Corrupti, atque consequatur? Ab aspernatur temporibus, ullam
 				quis doloremque quo! Ratione a voluptatem ullam aliquid est
-				iusto quod incidunt aspernatur, fugit necessitatibus!
+				iusto quod incidunt aspernatur, fugit necessitatibus! Lorem
+				ipsum dolor sit, amet consectetur adipisicing elit. Assumenda,
 			</p>
-		</div>
+			{type === "old" && (
+				<div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none"></div>
+			)}
+		</button>
 	);
 };
-
